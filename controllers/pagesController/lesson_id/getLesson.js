c424398b9357291c1f55f5b85185c04613be88d5
vleypi <vleypi@gmail.com>
2022-12-18 18:28:38 +0500

    fix

--- controllers/pagesController/lesson_id/getLesson.js
+++ controllers/pagesController/lesson_id/getLesson.js
@@ -5,7 +5,7 @@ import nookies from 'nookies'
 export const getLessonParticipants = async (ctx) =>{
     try{
         const cookies = nookies.get(ctx)
-        const res = await axios.get/(`http://localhost:5001/api/pages/getLesson/participants?lesson_id=${ctx.params.lesson_id }`, {
+        const res = await axios.get(`http://localhost:5001/api/pages/getLesson/participants?lesson_id=${ctx.params.lesson_id }`, {
             data: {
                 ref: cookies.ref ? cookies.ref : '',
                 acc: cookies.acc ? cookies.acc : '',
